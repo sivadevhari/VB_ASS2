9a70be2b86b2294510356cd028fd9821af1a3d61
Sivadev Hariprasad <165980772+sivadevhari@users.noreply.github.com>
2024-05-16 10:19:57 +0530

    Traffic Signal

---
+++ Form1.vb
@@ -0,0 +1,30 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        PictureBox1.Visible = True
+        PictureBox2.Visible = False
+        PictureBox3.Visible = False
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        If PictureBox1.Visible = True Then
+            PictureBox1.Visible = False
+            PictureBox2.Visible = True
+            PictureBox3.Visible = False
+        ElseIf PictureBox2.Visible = True Then
+            PictureBox1.Visible = False
+            PictureBox2.Visible = False
+            PictureBox3.Visible = True
+        ElseIf PictureBox3.Visible = True Then
+            PictureBox1.Visible = True
+            PictureBox2.Visible = False
+            PictureBox3.Visible = False
+        End If
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Timer1.Interval = 2000
+        Timer1.Start()
+        Button1.Enabled = False
+    End Sub
+End Class
