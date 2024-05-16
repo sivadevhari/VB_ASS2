d16c9be5186ff1fb1da68a8ce4acdea462c1f24e
Sivadev Hariprasad <165980772+sivadevhari@users.noreply.github.com>
2024-05-16 10:20:30 +0530

    Add files via upload
    Tab control

--- Form1.vb
+++ Form1.vb
@@ -1,30 +1,17 @@
 ﻿Public Class Form1
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        PictureBox1.Visible = True
-        PictureBox2.Visible = False
-        PictureBox3.Visible = False
-    End Sub
-
-    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
-        If PictureBox1.Visible = True Then
-            PictureBox1.Visible = False
-            PictureBox2.Visible = True
-            PictureBox3.Visible = False
-        ElseIf PictureBox2.Visible = True Then
-            PictureBox1.Visible = False
-            PictureBox2.Visible = False
-            PictureBox3.Visible = True
-        ElseIf PictureBox3.Visible = True Then
-            PictureBox1.Visible = True
-            PictureBox2.Visible = False
-            PictureBox3.Visible = False
-        End If
-
+        Button1.text="CLICK"
+        Button2.Text = "CLICK"
+        Label1.Text = " "
     End Sub
 
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
-        Timer1.Interval = 2000
-        Timer1.Start()
-        Button1.Enabled = False
+        Label1.Text = "Button from tab 1 is clicked!!"
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Label1.Text = "Button from tab 2 is clicked!!"
     End Sub
 End Class
+
+
